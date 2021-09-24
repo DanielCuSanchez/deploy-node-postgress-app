f2bb0168d150f774d22809a063dd1bfd5cedafb5
Daniel Cu Sanchez <danielcu@alternet.com.mx>
2021-09-23 22:09:13 -0500

    change port

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import app from './app';
 
-async function main() {
-	await app.listen(3001);
-	console.log('http://localhost:3001/');
+function main() {
+	const port = process.env.PORT || 3001;
+	app.listen(port, () => {
+		console.log(`Listening on http://localhost:${port}/`);
+	});
 }
 
 main();
